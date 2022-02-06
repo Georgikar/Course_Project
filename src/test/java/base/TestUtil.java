ce317f9ece4a4e322adbe64413aff32b6708a2e5
Georgi <georgi.karpovchanski@yahoo.com>
2022-02-06 20:59:11 +0200

    Update

--- src/test/java/base/TestUtil.java
+++ src/test/java/base/TestUtil.java
@@ -1,66 +1,51 @@
 package base;
 
 import Driver.DriverFactory;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 import java.util.Properties;
 
 public class TestUtil {
 
     public WebDriver driver;
-    private String applicationUrl,browser;
+    private String applicationUrl, browser;
     private int implicitWaitSeconds;
 
-    @DataProvider(name = "usersCsv")
-    public static Object[][] readUsersFromCsv() throws IOException, CsvException {
-        try (CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/userList.csv"))) {
-            List<String[]> csvData = csvReader.readAll();
-            Object[][] csvDataObject = new Object[csvData.size()][2];
-            for (int i = 0; i < csvData.size(); i++) {
-                csvDataObject[i] = csvData.get(i);
-            }
-            return csvDataObject;
-        }
-    }
+
     @BeforeMethod
     public void setUp() {
-    setupBrowserDriver();
-    loadInitialPage();
+        setupBrowserDriver();
+        loadInitialPage();
     }
 
-    private void loadInitialPage() {driver.get(applicationUrl);}
+    private void loadInitialPage() {
+        driver.get(applicationUrl);
+    }
 
     @AfterMethod
-
     public void tearDown() {
-    driver.quit();
+        driver.quit();
 
     }
 
     private void setupBrowserDriver() {
-    try {
-        FileInputStream configFile = new
-                FileInputStream("src/test/resources/config.properties");
-        Properties config = new Properties();
-        config.load(configFile);
-        applicationUrl = config.getProperty("url");
-        //implicitWaitSeconds = Integer.parseInt(config.getProperty("implicitWait"));
-        browser = config.getProperty("targetBrowser");
+        try {
+            FileInputStream configFile = new
+                    FileInputStream("src/test/resources/config.properties");
+            Properties config = new Properties();
+            config.load(configFile);
+            applicationUrl = config.getProperty("url");
+            //implicitWaitSeconds = Integer.parseInt(config.getProperty("implicitWait"));
+            browser = config.getProperty("targetBrowser");
 
-    }
-    catch (IOException e) {
-        e.printStackTrace();
-            }
-        switch (browser){
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        switch (browser) {
             case "chrome":
                 driver = DriverFactory.getChromeDriver();
                 break;
@@ -69,6 +54,6 @@ public class TestUtil {
                 break;
         }
     }
-    }
+}
 
 
---
+++ src/test/java/com/course/tests/UnsuccessfulLogin.java
@@ -0,0 +1,46 @@
+package com.course.tests;
+
+import base.TestUtil;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class UnsuccessfulLogin extends TestUtil {
+
+    @DataProvider(name = "userList")
+    public static Object[][] readUsersFromCsv() throws IOException, CsvException {
+        try (CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/userList.csv"))) {
+            List<String[]> csvData = csvReader.readAll();
+            Object[][] csvDataObject = new Object[csvData.size()][2];
+            for (int i = 0; i < csvData.size(); i++) {
+                csvDataObject[i] = csvData.get(i);
+            }
+            return csvDataObject;
+        }
+    }
+
+    @Test(dataProvider = "userList")
+    public void unsuccessfulLogin(String userName, String password)  {
+        WebElement userNameInput = driver.findElement(By.id("user-name"));
+        userNameInput.sendKeys(userName);
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("[placeholder=Password]"));
+        passwordInput.sendKeys(password);
+
+        WebElement loginBtn = driver.findElement(By.name("login-button"));
+        loginBtn.click();
+
+
+        WebElement wrongUserBtn = driver.findElement(By.cssSelector(".error-button"));
+
+        Assert.assertTrue(wrongUserBtn.isDisplayed());
+    }
+}
