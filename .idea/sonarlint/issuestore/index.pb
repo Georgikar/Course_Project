6b873f204ab862cd59e61100d68c67c61d315849
Georgi <georgi.karpovchanski@yahoo.com>
2022-03-16 19:23:18 +0200

    Update3

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
-P
- src/test/java/base/TestUtil.java,5\9\59d9ef061498841be504356267b8ae8810e418b8
 R
 "src/main/java/Utils/HelperCsv.java,3\f\3f451a5f53f5b1ce1300513ab85fb2d99f2926e8
 ^
--- src/test/java/com/course/tests/AddingItems.java
+++ src/test/java/com/course/tests/AddingItems.java
@@ -1,4 +1,42 @@
 package com.course.tests;
 
-public class AddingItems {
+import Pages.LoginPage;
+import Pages.ProductsPage;
+import base.TestUtil;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class AddingItems extends TestUtil {
+
+
+    @DataProvider(name = "userList")
+    public static Object[][] readUsersFromCsv() throws IOException, CsvException {
+        try (CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/userList.csv"))) {
+            List<String[]> csvData = csvReader.readAll();
+            Object[][] csvDataObject = new Object[csvData.size()][2];
+            for (int i = 0; i < csvData.size(); i++) {
+                csvDataObject[i] = csvData.get(i);
+            }
+            return csvDataObject;
+        }
+    }
+    @Test(dataProvider = "userList")
+    public void addItemToTheCart(String userName,String password){
+
+        LoginPage loginPage = new LoginPage(driver);
+        ProductsPage productPage = loginPage.login(userName,password);
+        productPage.addToCartByProductName("backpack");
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(ProductsPage.;
+    }
+
+
 }
