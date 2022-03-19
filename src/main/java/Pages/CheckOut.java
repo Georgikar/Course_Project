a89a073aa54b8f622276d82597c7321be20b57a8
Georgi <georgi.karpovchanski@yahoo.com>
2022-03-19 16:30:38 +0200

    Update4

--- src/main/java/Pages/CheckOut.java
+++ src/main/java/Pages/CheckOut.java
@@ -1,11 +1,37 @@
 package Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class CheckOut {
 
     protected WebDriver driver;
 
+    @FindBy(name="firstName")
+    private WebElement firstNameInput;
+
+    @FindBy(name="lastName")
+    private WebElement lastNameInput;
+
+    @FindBy(name="postalCode")
+    private WebElement postalCodeInput;
+
+    public CheckOut(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    public void informationInput(String firstName, String lastName, int postalCode){
+        firstNameInput.clear();
+        firstNameInput.sendKeys(firstName);
+
+        lastNameInput.clear();
+        lastNameInput.sendKeys(lastName);
+
+        postalCodeInput.clear();
+        postalCodeInput.sendKeys(postalCode);
 
 
+    }
 }
